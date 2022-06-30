89bd9508dc8c42a2f1537c4a53a55c1f7f17bdf0
moniruzzaman01 <moniruzzamanshakib04@gmail.com>
2022-06-30 11:39:35 +0600

    api skeleton created

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
+const { MongoClient, ServerApiVersion } = require("mongodb");
 const express = require("express");
 const cors = require("cors");
+require("dotenv").config();
 const app = express();
 const port = process.env.PORT || 5000;
 
@@ -12,3 +14,28 @@ app.get("/", (req, res) => {
 app.listen(port, () => {
   console.log("listening from", port);
 });
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.jwrz65q.mongodb.net/?retryWrites=true&w=majority`;
+// const uri = `mongodb+srv://PHero_task_user:pwci1tkDsg2RVXZY@cluster0.jwrz65q.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+
+async function run() {
+  try {
+    await client.connect();
+    const billingCollection = client.db("PHero").collection("billing-list");
+
+    app.post("/registration", async (req, res) => {});
+    app.get("/login", async (req, res) => {});
+    app.get("/billing-list", async (req, res) => {});
+    app.post("/add-billing", async (req, res) => {});
+    app.post("/update-billing/:id", async (req, res) => {});
+    app.post("/delete-billing/:id", async (req, res) => {});
+  } finally {
+    // await client.close();
+  }
+}
+run().catch(console.dir);
